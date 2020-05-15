0c9ff57996bcbb19a1eb4dcfac194a527e815993
Mamadou Abdarahim Anne <mmdanne98@gmail.com>
2020-05-15 15:10:29 +0000

    first commit de a partie back end du projet

---
+++ src/main/java/com/iscae/alpha/pgp/AlphaPgpApplication.java
@@ -0,0 +1,13 @@
+package com.iscae.alpha.pgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaPgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaPgpApplication.class, args);
+	}
+
+}
